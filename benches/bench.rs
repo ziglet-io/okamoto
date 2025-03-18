adad3ce6155abf248dfe965c3d73fce199eb14ff
Jeffrey Hohenstein <jeffrey.hohenstein@gmail.com>
2025-03-18 15:48:57 -0500

    fix bench

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,9 +1,9 @@
-use std::time::Duration;
 use bls12_381::Scalar;
 use criterion::{criterion_group, criterion_main, Criterion};
 use ff::Field;
-use rand::prelude::ThreadRng;
+use rand_core::OsRng;
 use simple_logger::SimpleLogger;
+use std::time::Duration;
 use ziglet_okamoto::bls12_381_plain::{verify_signature, KeyPair, Signer, User};
 
 #[allow(non_snake_case)]
@@ -15,14 +15,14 @@ fn bench(criterion: &mut Criterion) {
     group.sample_size(1000);
 
     group.bench_function("generate", |b| {
-        let mut rng = ThreadRng::default();
+        let mut rng = OsRng;
         b.iter(|| {
             let _ = KeyPair::generate(&mut rng);
         });
     });
 
     group.bench_function("sign", |b| {
-        let mut rng = rand::thread_rng();
+        let mut rng = OsRng;
         let key_pair = KeyPair::generate(&mut rng);
         let m0 = Scalar::random(&mut rng);
         let m1 = Scalar::random(&mut rng);
@@ -32,37 +32,35 @@ fn bench(criterion: &mut Criterion) {
             let mut signer = Signer::new(&key_pair, rng.clone());
             user.set_message(m0, m1).unwrap();
             signer.set_message(m0).unwrap();
-            let (W,X) = user.commit().unwrap();
+            let (W, X) = user.commit().unwrap();
             let eta = signer.commit(W, X).unwrap();
             let (b1, b2, b3) = user.compute_witness(&eta).unwrap();
             signer.verify_witness(b1, b2, b3).unwrap();
-            let (Y,R,l) = signer.sign().unwrap();
-            user.sign(&Y,&R,&l).unwrap();
+            let (Y, R, l) = signer.sign().unwrap();
+            user.sign(&Y, &R, &l).unwrap();
         });
     });
 
     group.bench_function("verify", |b| {
-        let mut rng = rand::thread_rng();
+        let mut rng = OsRng;
         let key_pair = KeyPair::generate(&mut rng);
-        let mut rng = rand::thread_rng();
         let m0 = Scalar::random(&mut rng);
         let m1 = Scalar::random(&mut rng);
         let mut user = User::new(&key_pair.public_key, rng.clone());
         let mut signer = Signer::new(&key_pair, rng.clone());
         user.set_message(m0, m1).unwrap();
         signer.set_message(m0).unwrap();
-        let (W,X) = user.commit().unwrap();
+        let (W, X) = user.commit().unwrap();
         let eta = signer.commit(W, X).unwrap();
         let (b1, b2, b3) = user.compute_witness(&eta).unwrap();
         signer.verify_witness(b1, b2, b3).unwrap();
-        let (Y,R,l) = signer.sign().unwrap();
-        let (sigma, alpha, beta) = user.sign(&Y,&R,&l).unwrap();
+        let (Y, R, l) = signer.sign().unwrap();
+        let (sigma, alpha, beta) = user.sign(&Y, &R, &l).unwrap();
 
         b.iter(|| {
             verify_signature(&key_pair.public_key, &m0, &m1, &sigma, &alpha, &beta).unwrap();
         });
     });
-
 }
 
 criterion_group!(benches, bench);
